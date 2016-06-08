888f02eea3df8ed4d0315de79e67acf074ae8748
Scott Morgan <scott@scottmorgan.ca>
2016-06-08 21:09:01 +0000

    Fixed margins related to header

--- themes/custom/vandocs/js/script.js
+++ themes/custom/vandocs/js/script.js
@@ -1,4 +1,4 @@
- $('.stm-menu').on('click', function(){
-           $('nav ul').toggleClass('showing'); 
+$( '<img id="hamburger" src="https://c9test-stmorgan.c9users.io/themes/custom/vandocs/images/hamburger.png" alt="hamburger" />' ).insertAfter( "#block-vandocs-branding" );
+$('#hamburger').on('click', function(){
+           $('#block-vandocs-main-menu ul').toggleClass('showing'); 
         });
-        
